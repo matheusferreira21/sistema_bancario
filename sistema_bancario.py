7408253260f021f0638c4e636f23b8d291cc0013
matheusferreira21 <matheusferreira034@hotmail.com>
2024-05-25 22:18:18 -0300

    Adicionando as funções criar_cliente, listar_cliente, criar_conta_corrente, listar_conta_corrente

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,5 +1,7 @@
+#No criar usuário, falta verificar se o cpf é so numero
 
 
+#Função de deposito
 def deposito(saldo, valor, extrato):
     if valor >= 0:
         saldo += valor
@@ -9,7 +11,7 @@ def deposito(saldo, valor, extrato):
         print("O valor do depósito precisar ser um número positivo.")
     return saldo,extrato
 
-
+# Função de saque com verficação
 def saque(valor_saque, limite, saldo, numero_saques, LIMITE_SAQUE, extrato):
     if valor_saque >=0 and valor_saque <= limite:
         if saldo >= valor_saque:
@@ -27,6 +29,7 @@ def saque(valor_saque, limite, saldo, numero_saques, LIMITE_SAQUE, extrato):
 
     return saldo, extrato, numero_saques
 
+# Mostrar o extrato
 def mostrar_extrato(extrato,saldo):
     if extrato == "":
         print("Não foi realizado nenhuma transação.")
@@ -35,16 +38,92 @@ def mostrar_extrato(extrato,saldo):
         print(extrato)
         print(f"\nSaldo: {saldo}")
 
+# Criar novo cliente
 def criar_usuario():
-    print("criar usuario")
+    usuario = []
+    endereco = ""
+    cpf = ""
 
-def criar_conta_corrent():
-    print("criar conta_corrente")
+    usuario.append(input("digite o nome do cliente: "))
+    usuario.append(input("digite a data de nascimento: "))
+
+    while True:
+        cpf = input("digite o CPF: ")
+
+        if verificar_cpf(cpf) == 1 or cpf.isnumeric() == False:
+            print("O CPF digitado é inválido")
+        else:
+            usuario.append(cpf)
+            break
+
+    endereco += input("digite o logradouro: ")+", "
+    endereco += input("digite o número do endereço: ") + " - "
+    endereco += input("digite o bairro: ") + " - "
+    endereco += input("digite a cidade: ") + "/"
+    endereco += input("digite a sigla do estado: ")
+
+    usuario.append(endereco)
+    print("O cliente foi cadastrado com sucesso!")
+    return usuario
+
+#listar usuarios
+def lista_usuarios():
+    contador = 0
+    for usuario in usuarios:
+        print(str(contador) + f": Nome do cliente {usuario[0]}, data de nascimento: {usuario[1]}, CPF: {usuario[2]}, Endereço: {usuario[3]} ")
+        contador += 1
+
+# Verificar se o cpf já existe
+def verificar_cpf(cpf):
+    for usuario in usuarios:
+        tem_quantos_cpf_iguais = usuario[2].count(cpf)
+        if tem_quantos_cpf_iguais == 1:
+            return 1
+    return 0
+
+#criar conta corrente
+def criar_conta_corrente(numero_conta):
+    NUMERO_AGENCIA = "0001"
+    conta = []
+    cpf = ""
+
+    conta.append("Agência: "+ NUMERO_AGENCIA)
+    conta.append("Numero da conta: "+ str(numero_conta))
+
+    while True:
+        cpf = input("Digite o CPF do cliente: ")
+
+        if verificar_cpf(cpf) == 1:
+            conta.append(cpf)
+            break
+        else:
+            print("O CPF digitado não existe.")
+    
+    print("Conta corrente criada com sucesso")
+    return conta
+
+
+def listar_conta_correntes():
+    for conta in contas:
+        for usuario in usuarios:
+            if usuario[2] == conta[2]:
+                print(conta[0] + ", " + conta[1]+", Nome do titular: "+ usuario[0])
 
 menu = """
+#### Cliente ####
+
+[u] criar cliente
+[c] criar conta corrente
+[l] listar cliente
+[r] listar contas correntes
+
+##### Acões #####
+
 [d] depositar
 [s] sacar
 [e] extrato
+
+#################
 [q] sair
 
 => """
@@ -54,7 +133,9 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUE = 3
-lista_usuarios = {}
+usuarios = []
+contas = []
+numero_conta = 1
 
 while True:
     
@@ -73,6 +154,18 @@ while True:
     elif opcao == "e":
         mostrar_extrato(extrato,saldo=saldo)
         
+    elif opcao == "u":
+        usuarios.append(criar_usuario())
+
+    elif opcao == "c":
+        contas.append(criar_conta_corrente(numero_conta))
+        numero_conta += 1
+
+    elif opcao == "l":
+        lista_usuarios()
+
+    elif opcao == "r":
+        listar_conta_correntes()
 
     elif opcao == "q":
         break
